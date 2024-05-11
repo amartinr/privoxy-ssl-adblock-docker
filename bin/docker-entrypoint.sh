c0ff10e2f537606ed36a33c6f441e468f2309b24
Abel Martín <abel.martin.ruiz@gmail.com>
2024-05-11 22:45:36 +0200

    Generate privoxy-blocklist configuration 'on the fly'

--- bin/docker-entrypoint.sh
+++ bin/docker-entrypoint.sh
@@ -12,9 +12,23 @@ function create_cert() {
     cat $CA_CERT
 }
 
+# issue CA certificate if missing
 [ -f ${CA_CERT} ] && [ -f ${CA_KEY} ] || create_cert
+
+# privoxy-bloklist URLs and filters
+URLS=()
+for URL in ${ADBLOCK_URLS}; do
+    URLS+=(${URL})
+done
+echo URLS="(${URLS[@]})" > /usr/local/etc/privoxy-blocklist/adblock-dyn.conf
+
+FILTERS=()
+for FILTER in ${ADBLOCK_FILTERS}; do
+    FILTERS+=(${FILTER})
+done
+echo FILTERS="(${FILTERS[@]})" >> /usr/local/etc/privoxy-blocklist/adblock-dyn.conf
 
 /usr/local/bin/privoxy-blocklist.sh \
     -c /usr/local/etc/privoxy-blocklist/privoxy-blocklist.conf
 
-/usr/local/sbin/privoxy --no-daemon /usr/local/etc/privoxy/config
+exec "$@"
